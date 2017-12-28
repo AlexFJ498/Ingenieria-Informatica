4731105252b0c8ab666ab1545dec5cb15c1c3384
AlexFJ498 <34347787+AlexFJ498@users.noreply.github.com>
2017-12-28 14:49:40 +0100

    Create ejercicio7.c

---
+++ so/practica2/ejercicio7.c
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define N_PROVEEDORES 5  //NÚMERO DE PROVEEDORES/MODELOS DE CAMISETA DEL EJERCICIO
+
+int camisetas[N_PROVEEDORES];
+pthread_mutex_t mtx=PTHREAD_MUTEX_INITIALIZER;
+
+void *simulaCompra(void *contador);
+void *simulaSuministro(void *contador);
+
+int main(){
+	int n;
+	long i;
+	srand(time(NULL));
+
+	//Generar n clientes
+	printf("Número de clientes: ");
+	scanf("%d",&n);
+	pthread_t cliente[n];
+
+	//Generar proveedores
+	pthread_t proveedores[N_PROVEEDORES];
+	
+	//crear el stock de cada modelo (cantidad inicial de camisetas: 500)
+	printf("Valores iniciales:\n");
+	for(i=0;i<N_PROVEEDORES;i++){
+		camisetas[i]=500;
+		printf("camisetas[%ld]=500\n",i);
+	}
+	printf("\nClientes: %d\n",n);
+	printf("Proveedores: %d\n\n",N_PROVEEDORES);
+	
+	//hilos Clientes
+	for(i=0;i<n;i++){ 
+
+		pthread_create(&cliente[i],NULL,(void *)simulaCompra,(void *)i);
+	}
+
+	//hilos Suministros
+	for(i=0;i<N_PROVEEDORES;i++){
+
+		pthread_create(&proveedores[i],NULL,(void *)simulaSuministro,(void *)i);
+	}
+
+	//Cerrar hilos
+	for(i=0;i<n;i++){
+		pthread_join(cliente[i],NULL);
+	}
+	for(i=0;i<N_PROVEEDORES;i++){
+		pthread_join(proveedores[i],NULL);
+	}
+
+	//Imprimir el número de camisetas finales
+	printf("\nResultado final:\n");
+	for(i=0;i<N_PROVEEDORES;i++){
+		printf("camisetas[%ld]=%d\n",i,camisetas[i]);
+	}
+	pthread_mutex_destroy(&mtx);
+}
+
+void *simulaCompra(void *contador){
+	int m,c,i;
+	int s;
+
+	printf("Cliente %ld: ",(long)contador);
+
+	//valor aleatorio para el modelo de camiseta
+	m=rand()%N_PROVEEDORES;
+	
+	//valor aleatorio para la cantidad a comprar (máximo 100)
+	c=rand()%100;
+	
+	s=pthread_mutex_lock(&mtx);
+		/*Sección crítica*/
+		printf("camisetas[%d]=%d-%d=",m,camisetas[m],c);
+		camisetas[m]-=c;
+		printf("%d\n",camisetas[m]);
+	s=pthread_mutex_unlock(&mtx);
+
+	return NULL;
+}
+
+void *simulaSuministro(void *contador){
+	int m,c,i;
+	int s;
+	
+	printf("Proveedor %ld: ",(long)contador);
+
+	//valor aleatorio para el modelo de camiseta
+	m=rand()%N_PROVEEDORES;
+
+	//valor aleatorio para la cantidad a incrementar
+	c=rand()%100;
+
+	s=pthread_mutex_lock(&mtx);
+		/*Sección crítica*/
+		printf("camisetas[%d]=%d+%d=",m,camisetas[m],c);
+		camisetas[m]+=c;
+		printf("%d\n",camisetas[m]);
+	s=pthread_mutex_unlock(&mtx);
+
+	return NULL;
+}
