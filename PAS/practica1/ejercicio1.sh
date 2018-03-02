7530143fcebfcb085c70e170aecffb44e2db62bc
AlexFJ498 <34347787+AlexFJ498@users.noreply.github.com>
2018-03-02 13:03:36 +0000

    Create ejercicio1.sh

---
+++ PAS/practica1/ejercicio1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $# != 1 ]
+then
+	echo "Error, debes incluir un directorio como parametro"
+	exit
+else
+	cd $1
+	echo "Tenemos $(find -name "*.c" | wc -l) ficheros con extension .c y $(find -name "*.h" | wc -l) ficheros con extension .h"
+	for dir in $(find -name "*.c" -o -name "*.h")
+	do
+		lin=$(wc -l < $dir)
+		car=$(wc -m < $dir)
+		echo "El fichero $dir tiene $lin lineas y $car caracteres"
+	done | sort -nr -k 8
+fi
+
