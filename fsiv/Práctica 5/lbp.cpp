2a4b52c75dc2ef96793de2ca189c10ffd035c313
alejandro <alejfuejur@gmail.com>
2019-12-13 13:00:14 +0100

    Fin de la práctica

--- fsiv/Práctica 5/lbp.cpp
+++ fsiv/Práctica 5/lbp.cpp
@@ -3,6 +3,7 @@
 
 #include "lbp.h"
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -20,12 +21,85 @@ static const int uniform[256] =
 58,58,58,50,51,52,58,53,54,55,56,57
 };
 
-
 void fsiv_lbp(const cv::Mat & imagem, cv::Mat & lbp)
 {
-    lbp = cv::Mat::zeros(imagem.rows, imagem.cols, CV_8UC1);
-	//TODO compute lbp codes.
-	
+	for(int i = 1; i < (imagem.rows-1); i++){
+		for(int j=1;j<(imagem.cols-1);j++){
+			int x = i; 
+			int y = j; 
+
+			unsigned char value = imagem.at<uchar>(x,y); 
+			unsigned char aux; 
+			unsigned char result = 0; 
+
+			//Pixel 0
+			aux = imagem.at<uchar>(x,y-1);
+			if(aux > value)
+				result = result | (1 << 7);
+			else
+				result = result | (0 << 7);
+
+			//Pixel 1
+			aux = imagem.at<uchar>(x+1,y-1);
+			if(aux > value)
+				result = result | (1 << 6);
+			else
+				result = result | (0 << 6);
+
+			//Pixel 2
+			aux = imagem.at<uchar>(x+1,y);
+			if(aux > value)
+				result = result | (1 << 5);
+			else
+				result = result | (0 << 5);
+
+			//Pixel 3
+			aux = imagem.at<uchar>(x+1,y+1);
+			if(aux > value)
+				result = result | (1 << 4);
+			else
+				result = result | (0 << 4);
+
+			//Pixel 4
+			aux = imagem.at<uchar>(x,y+1);
+			if(aux > value)
+				result = result | (1 << 3);
+			else
+				result = result | (0 << 3);
+
+			//Pixel 5
+			aux = imagem.at<uchar>(x-1,y+1);
+			if(aux > value)
+				result = result | (1 << 2);
+			else
+				result = result | (0 << 2);
+
+			//Pixel 6
+			aux = imagem.at<uchar>(x-1,y);
+			if(aux > value)
+				result = result | (1 << 1);
+			else
+				result = result | (0 << 1);
+
+			//Pixel 7
+			aux = imagem.at<uchar>(x-1,y-1);
+			if(aux > value)
+				result = result | (1 << 0);
+			else
+				result = result | (0 << 0);
+
+			lbp.at<uchar>(x,y) = result;
+		}
+	}
+
+	for(int i=0;i<imagem.rows;i++){
+		lbp.at<uchar>(i,0) = 0;
+		lbp.at<uchar>(i,lbp.cols-1) = 0;
+	}
+	for(int i=0;i<imagem.cols;i++){
+		lbp.at<uchar>(0,i) = 0;
+		lbp.at<uchar>(lbp.rows-1,i) = 0;
+	}
 }
 
 void fsiv_ulbp(const cv::Mat & imagem, cv::Mat & ulbp)
@@ -36,37 +110,101 @@ void fsiv_ulbp(const cv::Mat & imagem, cv::Mat & ulbp)
 
 void fsiv_lbp_hist(const cv::Mat & lbp, cv::Mat & lbp_hist, bool normalize, int nbins)
 {	    
-	 lbp_hist == cv::Mat::zeros(1, nbins, CV_32FC1);
-    //TODO compute lbp hist. \see cv::calcHist. Establish the number of bins
+	lbp_hist == cv::Mat::zeros(1, nbins, CV_32FC1);
 
-    // Normalize?
-    if (normalize)
-    {
-		//TODO normalize histogram to meet cv::sum(lbp_hist)[0] == 1.0
-		;
-    }
+	float range[] = {0,256};
+	const float* histRange = {range};
+
+	calcHist(&lbp, 1, 0, cv::Mat(), lbp_hist, 1, &nbins, &histRange);
+
+	//Normalizar
+	if(normalize == true){
+		float num = lbp.rows * lbp.cols;
+		lbp_hist.convertTo(lbp_hist, CV_32FC1);
+		for(int i = 0; i < nbins; i++){
+			lbp_hist.at<float>(i) = (lbp_hist.at<float>(i)/num);
+		}
+	}
+
+	lbp_hist = lbp_hist.t();
 }
 
 void fsiv_lbp_desc(const cv::Mat & image, cv::Mat & lbp_desc, const int *ncells, bool normalize, int nbins)
 {
-	//TODO compute lbp desc.
-    if (normalize)
-    {
-		//TODO normaliza lbp_desc to mett cv::sum(lbp_desc)[0] == 1.0
-		;
-    }
+	int nx = ncells[0]; 
+	int ny = ncells[1]; 
+	vector <cv::Mat> vector;
+
+	int rows = image.rows; 
+	int cols = image.cols; 
+	int c = cols/nx; 
+	int r = rows/ny; 
+
+	int numero = 0;
+	for(int i = 1; i <= ny; i++){ 
+		for(int j = 1; j <= nx; j++){ 
+			numero += 1;
+
+			int oldx = (j-1)*c;
+			int oldy = (i-1)*r;
+			int newx = j*c;
+			int newy = i*r;
+			int ccopy = c;
+			int rcopy = r;
+
+			if(j == nx)
+				ccopy += cols%nx; 
+			if(i == ny)
+				rcopy += rows%ny; 
+
+			cv::Rect rect(oldx, oldy, ccopy, rcopy);
+			cv::Mat region = image(rect);
+			cv::Mat aux = region.clone();
+			cv::Mat lbpregion;
+
+			fsiv_lbp(region, aux);
+			lbpregion = aux.clone();
+			fsiv_lbp_hist(aux, lbpregion, normalize, nbins);
+
+			if(numero == 1)
+				lbp_desc = lbpregion.clone();
+			else
+				hconcat(lbpregion, lbp_desc, lbp_desc);
+		}
+	}
 }
 
 void fsiv_lbp_disp(const cv::Mat & lbpmat, const std::string & winname)
 {
-	//TODO show lbpmat.
-	;
+	//show lbpmat.
+	cv::imshow(winname,lbpmat);
+
+	cv::waitKey(0);
 }
 
 float fsiv_chisquared_dist(const cv::Mat & h1, const cv::Mat & h2)
 {
 	 float dist= 0.0;
-	//TODO compute chi^2 distance. do not assume 256 bins allways!!
+	//compute chi^2 distance. do not assume 256 bins allways!!
+	float x,y,aux;
+
+	for(int i=0;i<h1.cols;i++){
+		x = (h1.at<float>(0,i) - h2.at<float>(0,i));
+		aux = pow(x,2);
+
+		x = aux;
+		y = (h1.at<float>(0,i) + h2.at<float>(0,i));
+
+		if(y == 0.0){
+			dist+=0.0;
+		}
+		else{
+			x = x/y;
+			dist+=x;
+		}
+	}
+	dist *=0.5;
+
     return dist;
 }
 
--- fsiv/Práctica 5/test_lbp.cpp
+++ fsiv/Práctica 5/test_lbp.cpp
@@ -15,8 +15,8 @@ using namespace std;
 const cv::String keys =
     "{help h usage ? |      | print this message.}"
 //    "{u uniform      |      | use uLBP.}"
-//    "{c colunms      |0     | Build descriptor using a grid with c colums.}"
-//    "{r rows         |0     | Build descriptor using a grid with r rows.}"
+    "{c colunms      |      | Build descriptor using a grid with c colums.}"
+    "{r rows         |      | Build descriptor using a grid with r rows.}"
     "{@image1        |<none>| path to input image.}"
     "{@image2        |      | path to second image, for comparison.}"
     ;
@@ -31,43 +31,58 @@ int main(int argc, char * argv[])
       return 0;
    }
 
-   // TODO Load the image1
-	cv::String nombre1 = parser.get<cv::String>(0);
-   cv::Mat image1;
-	image1 = cv::imread(nombre1);
+   //Load the image1
+    cv::Mat image1 = cv::imread(parser.get<cv::String>(0),0	);
 
-   // TODO Load the image2 (if it is set).
+   //Load the image2 (if it is set).
 	cv::String nombre2 = parser.get<cv::String>(1);
-   cv::Mat image2;
+    cv::Mat image2;
+
 	if(!nombre2.empty()){
-		image2 = cv::imread(nombre2);
+		image2 = cv::imread(nombre2,0);
 	}
-   
-	// TODO if image2 is not set, image2 = mirror(image1) (x axe). see cv::flip()	
+	//if image2 is not set, image2 = mirror(image1) (x axe). see cv::flip()	
 	else{
-		cv::flip(image1,image2);
+		cv::flip(image1,image2,0);
 	}
 
-   //  TODO Compute LBP matrixs
-	cv::Mat lbp1, lbp2;
-	fsiv_lbp(image1, lbp1);
-	fsiv_lbp(image2, lbp2);
-
-   // TODO Display LBP images
-	string window1 = "lbp1";
-	string window2 = "lbp2";
-	fsiv_lbp_disp(lbp1, window1);
-   	fsiv_lbp_disp(lbp2, window2);
-
-   // TODO Compute the LBP histograms.
-   cv::Mat lbp_h1, lbp_h2;
-	fsiv_lbp_hist(lbp1, lbp_h1);
-	fsiv_lbp_hist(lbp2, lbp_h2);
-   // TODO Compute the Chi^2 distance between the input image1 and image2, if it is set,
-   /// or else between image1 and its mirror.
 	float dist = 0.0;
+	if(parser.has("c") && parser.has("r")){
+		cv::Mat lbp_desc1 = image1.clone();
+		cv::Mat lbp_desc2 = image2.clone();
+		int ncells[2] = {0, 0}; 
 
-	dist = fsiv_chisquared_dist(lbp_h1, lbp_h2);
+		ncells[0] = parser.get<int>("c");
+		ncells[1] = parser.get<int>("r");
+
+		//Se realiza sobre la imagen1
+		fsiv_lbp_desc(image1, lbp_desc1, ncells);
+		fsiv_lbp_desc(image2, lbp_desc2, ncells);
+
+		dist = fsiv_chisquared_dist(lbp_desc1,lbp_desc2);
+	}
+	else{
+	   //Compute LBP matrixs
+		cv::Mat lbp1 = image1.clone();
+		cv::Mat lbp2 = image2.clone();
+
+		fsiv_lbp(image1, lbp1);
+		fsiv_lbp(image2, lbp2);
+
+  	 	//Display LBP images
+		fsiv_lbp_disp(lbp1, "lbp1");
+   		fsiv_lbp_disp(lbp2, "lbp2");
+
+	   //Compute the LBP histograms.
+	    cv::Mat lbp_h1, lbp_h2;
+		fsiv_lbp_hist(lbp1, lbp_h1);
+		fsiv_lbp_hist(lbp2, lbp_h2);
+	
+	   //Compute the Chi^2 distance between the input image1 and image2, if it is set,
+	   //or else between image1 and its mirror.
+	
+		dist = fsiv_chisquared_dist(lbp_h1, lbp_h2);
+	}
 
    std::cerr << "Distance between image1, image2 = " << dist << std::endl;   
 	std::cout << "End! " <<std::endl;
