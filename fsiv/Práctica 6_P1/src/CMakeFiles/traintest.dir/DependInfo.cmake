183f96a861ca0445d0b0b1e66279bc62054d269b
alejandro <alejfuejur@gmail.com>
2019-12-13 13:03:47 +0100

    Comienzo de la práctica

---
+++ fsiv/Práctica 6_P1/src/CMakeFiles/traintest.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/i62fujua/Desktop/fsiv/Práctica6_P1/src/common_code.cpp" "/home/i62fujua/Desktop/fsiv/Práctica6_P1/src/CMakeFiles/traintest.dir/common_code.cpp.o"
+  "/home/i62fujua/Desktop/fsiv/Práctica6_P1/src/lbp.cpp" "/home/i62fujua/Desktop/fsiv/Práctica6_P1/src/CMakeFiles/traintest.dir/lbp.cpp.o"
+  "/home/i62fujua/Desktop/fsiv/Práctica6_P1/src/traintest.cpp" "/home/i62fujua/Desktop/fsiv/Práctica6_P1/src/CMakeFiles/traintest.dir/traintest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include.ORIG"
+  "/usr/local/include.ORIG/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
